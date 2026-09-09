b598680ff28de6b84684ffe5e019e9377500564d
DivideX2 <dividex2business@gmail.com>
2026-09-10 00:00:38 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: ":YouTube:Subcribe To @Official M2 Gaming",     // this is what people actually see
+        state: "Subcribe To @Official M2 Gaming",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
